4ed010d642eca7c17a44f8d7e2a68698452b0dff
achrafaitibba <aitibbaachraf@gmail.com>
2023-12-22 10:48:02 +0100

    first commit

---
+++ src/main/java/com/achrafaitibba/trackcompoundingtrades/TrackCompoundingTradesApplication.java
@@ -0,0 +1,13 @@
+package com.achrafaitibba.trackcompoundingtrades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackCompoundingTradesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackCompoundingTradesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/achrafaitibba/trackcompoundingtrades/TrackCompoundingTradesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.achrafaitibba.trackcompoundingtrades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackCompoundingTradesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
