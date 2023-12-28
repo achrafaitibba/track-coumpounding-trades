be9f8861eba74e95eb452fe31debe62246683d02
achrafaitibba <aitibbaachraf@gmail.com>
2023-12-28 03:15:40 +0100

    h

--- src/main/java/com/achrafaitibba/trackcompoundingtrades/model/Account.java
+++ src/main/java/com/achrafaitibba/trackcompoundingtrades/model/Account.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
