ca19a0b8e0a183fab84b4fa1e3ca75758a346a70
achrafaitibba <aitibbaachraf@gmail.com>
2023-12-28 03:11:10 +0100

    calculate weekly targets

--- src/main/java/com/achrafaitibba/trackcompoundingtrades/service/AccountService.java
+++ src/main/java/com/achrafaitibba/trackcompoundingtrades/service/AccountService.java
@@ -26,10 +26,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -41,7 +38,7 @@ public class AccountService {
     private final JwtService jwtService;
     private final TokenRepository tokenRepository;
     private final CompoundingPeriodRepository compoundingPeriodRepository;
-    private final TargetRepository targetRepository;
+    private final TargetService targetService;
 
     public AccountRegisterResponse accountRegister(AccountRegisterRequest request) {
         /**
@@ -50,7 +47,7 @@ public class AccountService {
         if (userRepository.findByUsername(request.username()).isPresent()) {
             throw new RequestException(CustomErrorMessage.ACCOUNT_ALREADY_EXIST.getMessage(), HttpStatus.CONFLICT);
         }
-        List<Target> targets = calculateDailyTargets(request);
+        List<Target> targets = targetService.calculateWeeklyTargets(request);//targetService.calculateDailyTargets(request);
         CompoundingPeriod compoundingPeriod = compoundingPeriodRepository.save(CompoundingPeriod
                 .builder()
                 .number(request.compoundingPeriod().number())
@@ -78,7 +75,7 @@ public class AccountService {
                 .account(account)
                 .build());
 
-        //todo add claims "id, base capital.."
+        //todo add claims "id, baseCapital.."
         /** Instead of initiating an empty hashmap you can create a list of claims and add them to the hashmap
          Such as birthdate, account status... and any other data needed to be sent to the client whiting the token
          Example:
@@ -86,232 +83,15 @@ public class AccountService {
          currentDate.put("now", LocalDateTime.now()....);
          Claims could be : email, pictureLink, roles & groups , authentication time...
          */
-        var jwtToken = jwtService.generateToken(new HashMap<>(), toSave);
+        Map<String, Object> claims  = new HashMap<>();
+        claims.put("accountId", account.getAccountId());
+        var jwtToken = jwtService.generateToken(claims, toSave);
         var refreshToken = jwtService.generateRefreshToken(toSave);
         saveUserToken(toSave, jwtToken);
         return new AccountRegisterResponse(request.username(), jwtToken, refreshToken);
     }
 
-    /**
-     * Compounding period = (number * timeframe)
-     */
-    public int convertCompoundingPeriodToDays(TimeFrame timeFrame, Integer number) {
-        double timeFrameValue = 0;
-        switch (timeFrame) {
-            case DAY -> timeFrameValue = 1;
-            case MONTH -> timeFrameValue = 30.41;
-            case WEEK -> timeFrameValue = 7;
-            case YEAR -> timeFrameValue = 365;
-        }
-        return (int) Math.round((number * timeFrameValue));
-    }
 
-    //timeframe = days
-    // start date = today
-    // end date = today
-    // estimatedBalanceByTargetAndTimeFrame = check below
-    /**
-     * Based on
-     * base capital
-     * compound percentage
-     * estimated fees by trade %
-     * estimated loss possibilities %
-     * stop loss percentage
-     * current balance
-     */
-    public List<Target> calculateDailyTargets(AccountRegisterRequest request) {
-        List<Target> targets = new ArrayList<>();
-        int days = convertCompoundingPeriodToDays(request.compoundingPeriod().timeFrame(), request.compoundingPeriod().number());
-        double baseCapital = request.baseCapital();
-        double compoundPercentage = request.compoundPercentage()/100;
-        double estimatedFeesByTradePercentage = request.estimatedFeesByTradePercentage()/100;
-        int estimatedLossPossibilities = request.estimatedLossPossibilities();
-        double stopLossPercentage = request.stopLossPercentage()/100;
-        double currentBalance = baseCapital; // todo; (baseCapital + sum(profit by trades)
-        final int cycleLimit = request.tradingCycle();
-        LocalDate targetDate = request.officialStartDate();
-        ////////////////////////////////////::
-        int cycleLimitCount = cycleLimit; //days
-        int profitableTradesCount = 0; //>>>>>>>>>> (cycleLimit - estimatedLossPossibilities);
-        int losingTradesCount = 0 ; //>>>>>>>>>     (cycleLimit - profitableTradesCount);
-        for(var i = 0; i<days; i++){
-            if(cycleLimitCount<=cycleLimit & cycleLimitCount>0){
-                if(profitableTradesCount < (cycleLimit-estimatedLossPossibilities)){
-                    currentBalance = calculateTarget("win",currentBalance, compoundPercentage, estimatedFeesByTradePercentage);
-                    targets.add(
-                            Target.builder()
-                                    .timeFrame(TimeFrame.DAY)
-                                    .startDate(targetDate)
-                                    .endDate(targetDate)
-                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
-                                    .build()
-                    );
-                    targetDate = targetDate.plusDays(1);
-                    profitableTradesCount++;
-                }
-                else if(profitableTradesCount == (cycleLimit-estimatedLossPossibilities) & losingTradesCount < (cycleLimit-profitableTradesCount)){
-                    currentBalance = calculateTarget("loss",currentBalance, stopLossPercentage, estimatedFeesByTradePercentage);
-                    targets.add(
-                            Target.builder()
-                                    .timeFrame(TimeFrame.DAY)
-                                    .startDate(targetDate)
-                                    .endDate(targetDate)
-                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
-                                    .build()
-                    );
-                    targetDate = targetDate.plusDays(1);
-                    losingTradesCount++;
-
-                }
-                cycleLimitCount--;
-            }
-            else {
-                cycleLimitCount = cycleLimit - 1;
-                profitableTradesCount = 0;
-                losingTradesCount = 0;
-                if(profitableTradesCount < (cycleLimit-estimatedLossPossibilities)){
-                    currentBalance = calculateTarget("win",currentBalance, compoundPercentage, estimatedFeesByTradePercentage);
-                    targets.add(
-                            Target.builder()
-                                    .timeFrame(TimeFrame.DAY)
-                                    .startDate(targetDate)
-                                    .endDate(targetDate)
-                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
-                                    .build()
-                    );
-                    targetDate = targetDate.plusDays(1);
-                    profitableTradesCount++;
-                }
-                else if(profitableTradesCount == (cycleLimit-estimatedLossPossibilities) & losingTradesCount < (cycleLimit-profitableTradesCount)){
-                    currentBalance = calculateTarget("loss",currentBalance, stopLossPercentage, estimatedFeesByTradePercentage);
-                    targets.add(
-                            Target.builder()
-                                    .timeFrame(TimeFrame.DAY)
-                                    .startDate(targetDate)
-                                    .endDate(targetDate)
-                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
-                                    .build()
-                    );
-                    targetDate = targetDate.plusDays(1);
-                    losingTradesCount++;
-
-                }
-            }
-        }
-        targetRepository.saveAll(targets);
-        return targets;
-    }
-
-    public static void main(String[] args) {
-        AccountService a = AccountService.builder().build();
-        List<Target> targets = new ArrayList<>();
-        int days = 365;
-        double baseCapital = 400;
-        double compoundPercentage = 2/100;
-        double estimatedFeesByTradePercentage = 0.1/100;
-        int estimatedLossPossibilities = 10;
-        double stopLossPercentage = 2/100;
-        double currentBalance = baseCapital; // todo; (baseCapital + sum(profit by trades)
-        final int cycleLimit = 10;
-        LocalDate targetDate = LocalDate.parse("2023-01-01");
-        ////////////////////////////////////::
-        int cycleLimitCount = cycleLimit; //days
-        int profitableTradesCount = 0; //>>>>>>>>>> (cycleLimit - estimatedLossPossibilities);
-        int losingTradesCount = 0 ; //>>>>>>>>>     (cycleLimit - profitableTradesCount);
-        for(var i = 0; i<days; i++){
-            if(cycleLimitCount<=cycleLimit & cycleLimitCount>0){
-                if(profitableTradesCount < (cycleLimit-estimatedLossPossibilities)){
-                    currentBalance = a.calculateTarget("win",currentBalance, compoundPercentage, estimatedFeesByTradePercentage);
-                    targets.add(
-                            Target.builder()
-                                    .targetId((long) i)
-                                    .timeFrame(TimeFrame.DAY)
-                                    .startDate(targetDate)
-                                    .endDate(targetDate)
-                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
-                                    .build()
-                    );
-                    targetDate = targetDate.plusDays(1);
-                    profitableTradesCount++;
-                }
-                else if(profitableTradesCount == (cycleLimit-estimatedLossPossibilities) & losingTradesCount < (cycleLimit-profitableTradesCount)){
-                    currentBalance = a.calculateTarget("loss",currentBalance, stopLossPercentage, estimatedFeesByTradePercentage);
-                    targets.add(
-                            Target.builder()
-                                    .targetId((long) i)
-                                    .timeFrame(TimeFrame.DAY)
-                                    .startDate(targetDate)
-                                    .endDate(targetDate)
-                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
-                                    .build()
-                    );
-                    targetDate = targetDate.plusDays(1);
-                    losingTradesCount++;
-                }
-                cycleLimitCount--;
-            }
-            else {
-                cycleLimitCount = cycleLimit - 1;
-                profitableTradesCount = 0;
-                losingTradesCount = 0;
-                if(profitableTradesCount < (cycleLimit-estimatedLossPossibilities)){
-                    currentBalance = a.calculateTarget("win",currentBalance, compoundPercentage, estimatedFeesByTradePercentage);
-                    targets.add(
-                            Target.builder()
-                                    .targetId((long) i)
-                                    .timeFrame(TimeFrame.DAY)
-                                    .startDate(targetDate)
-                                    .endDate(targetDate)
-                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
-                                    .build()
-                    );
-                    targetDate = targetDate.plusDays(1);
-                    profitableTradesCount++;
-                }
-                else if(profitableTradesCount == (cycleLimit-estimatedLossPossibilities) & losingTradesCount < (cycleLimit-profitableTradesCount)){
-                    currentBalance = a.calculateTarget("loss",currentBalance, stopLossPercentage, estimatedFeesByTradePercentage);
-                    targets.add(
-                            Target.builder()
-                                    .targetId((long) i)
-                                    .timeFrame(TimeFrame.DAY)
-                                    .startDate(targetDate)
-                                    .endDate(targetDate)
-                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
-                                    .build()
-                    );
-                    targetDate = targetDate.plusDays(1);
-                    losingTradesCount++;
-                }
-            }
-        }
-
-        for (Target t:targets){
-            System.out.println(t.toString());
-        }
-    }
-    public double calculateTarget(String type, double currentBalance, double changePercentage, double estimatedFeesByTradePercentage){
-        switch (type){
-            case "loss":  currentBalance = currentBalance * (1 - changePercentage);
-            break;
-            case "win": currentBalance = currentBalance * (1 + changePercentage);
-            break;
-        }
-        currentBalance -= currentBalance * estimatedFeesByTradePercentage;
-        return currentBalance;
-    }
-
-    //streams on calculateDailyTargets, filtering or grouping by weeks
-    public List<Target> calculateWeeklyTargets() {
-        return null;
-    }
-    //streams on calculateDailyTargets, filtering or grouping by months
-    public List<Target> calculateMonthlyTargets() {
-        return null;
-    }
-    //streams on calculateDailyTargets, filtering or grouping by years
-    public List<Target> calculateYearlyTargets() {
-        return null;
-    }
     private void saveUserToken(User user, String jwtToken) {
         var token = Token.builder()
                 .user(user)
--- src/main/java/com/achrafaitibba/trackcompoundingtrades/service/TargetService.java
+++ src/main/java/com/achrafaitibba/trackcompoundingtrades/service/TargetService.java
@@ -1,25 +1,211 @@
 package com.achrafaitibba.trackcompoundingtrades.service;
 
+import com.achrafaitibba.trackcompoundingtrades.dto.request.AccountRegisterRequest;
+import com.achrafaitibba.trackcompoundingtrades.enumeration.TimeFrame;
 import com.achrafaitibba.trackcompoundingtrades.model.Target;
+import com.achrafaitibba.trackcompoundingtrades.repository.TargetRepository;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
 @Service
+@RequiredArgsConstructor
+@Builder
 public class TargetService {
 
+    private final TargetRepository targetRepository;
+
+    /**
+     * Compounding period = (number * timeframe)
+     */
+    public int convertCompoundingPeriodToDays(TimeFrame timeFrame, Integer number) {
+        double timeFrameValue = 0;
+        switch (timeFrame) {
+            case DAY -> timeFrameValue = 1;
+            case MONTH -> timeFrameValue = 30.41;
+            case WEEK -> timeFrameValue = 7;
+            case YEAR -> timeFrameValue = 365;
+        }
+        return (int) Math.round((number * timeFrameValue));
+    }
 
 
-    public List<Target> calculateDailyTargets(){
-        return null;
+    /**
+     * Based on
+     * base capital
+     * compound percentage
+     * estimated fees by trade %
+     * estimated loss possibilities %
+     * stop loss percentage
+     * current balance
+     */
+    public List<Target> calculateDailyTargets(AccountRegisterRequest request) {
+        List<Target> targets = new ArrayList<>();
+        int days = convertCompoundingPeriodToDays(request.compoundingPeriod().timeFrame(), request.compoundingPeriod().number());
+        double baseCapital = request.baseCapital();
+        double compoundPercentage = request.compoundPercentage() / 100;
+        double estimatedFeesByTradePercentage = request.estimatedFeesByTradePercentage() / 100;
+        int estimatedLossPossibilities = request.estimatedLossPossibilities();
+        double stopLossPercentage = request.stopLossPercentage() / 100;
+        double currentBalance = baseCapital; // todo; (baseCapital + sum(profit by trades)
+        final int cycleLimit = request.tradingCycle();
+        LocalDate targetDate = request.officialStartDate();
+        ////////////////////////////////////::
+        int cycleLimitCount = cycleLimit; //days
+        int profitableTradesCount = 0;
+        int losingTradesCount = 0;
+        for (var i = 0; i < days; i++) {
+            if (cycleLimitCount <= cycleLimit & cycleLimitCount > 0) {
+                if (profitableTradesCount < (cycleLimit - estimatedLossPossibilities)) {
+                    currentBalance = calculateTarget("win", currentBalance, compoundPercentage, estimatedFeesByTradePercentage);
+                    targets.add(
+                            Target.builder()
+                                    .timeFrame(TimeFrame.DAY)
+                                    .startDate(targetDate)
+                                    .endDate(targetDate)
+                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
+                                    .variableEstimatedBalanceByTargetAndTimeFrame(baseCapital)
+                                    .build()
+                    );
+                    targetDate = targetDate.plusDays(1);
+                    profitableTradesCount++;
+                } else if (profitableTradesCount == (cycleLimit - estimatedLossPossibilities) & losingTradesCount < (cycleLimit - profitableTradesCount)) {
+                    currentBalance = calculateTarget("loss", currentBalance, stopLossPercentage, estimatedFeesByTradePercentage);
+                    targets.add(
+                            Target.builder()
+                                    .timeFrame(TimeFrame.DAY)
+                                    .startDate(targetDate)
+                                    .endDate(targetDate)
+                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
+                                    .variableEstimatedBalanceByTargetAndTimeFrame(baseCapital)
+
+                                    .build()
+                    );
+                    targetDate = targetDate.plusDays(1);
+                    losingTradesCount++;
+
+                }
+                cycleLimitCount--;
+            } else {
+                cycleLimitCount = cycleLimit - 1;
+                profitableTradesCount = 0;
+                losingTradesCount = 0;
+                if (profitableTradesCount < (cycleLimit - estimatedLossPossibilities)) {
+                    currentBalance = calculateTarget("win", currentBalance, compoundPercentage, estimatedFeesByTradePercentage);
+                    targets.add(
+                            Target.builder()
+                                    .timeFrame(TimeFrame.DAY)
+                                    .startDate(targetDate)
+                                    .endDate(targetDate)
+                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
+                                    .variableEstimatedBalanceByTargetAndTimeFrame(baseCapital)
+                                    .build()
+                    );
+                    targetDate = targetDate.plusDays(1);
+                    profitableTradesCount++;
+                } else if (profitableTradesCount == (cycleLimit - estimatedLossPossibilities) & losingTradesCount < (cycleLimit - profitableTradesCount)) {
+                    currentBalance = calculateTarget("loss", currentBalance, stopLossPercentage, estimatedFeesByTradePercentage);
+                    targets.add(
+                            Target.builder()
+                                    .timeFrame(TimeFrame.DAY)
+                                    .startDate(targetDate)
+                                    .endDate(targetDate)
+                                    .estimatedBalanceByTargetAndTimeFrame(currentBalance)
+                                    .variableEstimatedBalanceByTargetAndTimeFrame(baseCapital)
+                                    .build()
+                    );
+                    targetDate = targetDate.plusDays(1);
+                    losingTradesCount++;
+
+                }
+            }
+        }
+        targetRepository.saveAll(targets);
+        return targets;
     }
-    public List<Target> calculateWeeklyTargets(){
+
+    public double calculateTarget(String type, double currentBalance, double changePercentage, double estimatedFeesByTradePercentage) {
+        switch (type) {
+            case "loss":
+                currentBalance = currentBalance * (1 - changePercentage);
+                break;
+            case "win":
+                currentBalance = currentBalance * (1 + changePercentage);
+                break;
+        }
+        currentBalance -= currentBalance * estimatedFeesByTradePercentage;
+        return currentBalance;
+    }
+    //streams on calculateDailyTargets, filtering or grouping by weeks
+
+    /**
+     * Weekly target : est of the latest day within the week
+     * target start date = first day of the week
+     * target end date = last day of the week OR start date + 6
+     * timeframe = week
+     * variable est balance = baseCapital // todo to be updated after every trade
+     */
+    public List<Target> calculateWeeklyTargets(AccountRegisterRequest request) {
+        List<Target> weeklyTargets = new ArrayList<>();
+        List<Target> dailyTargets = calculateDailyTargets(request);
+        int days = convertCompoundingPeriodToDays(request.compoundingPeriod().timeFrame(), request.compoundingPeriod().number());
+        int weeks = days/ 7;
+        LocalDate startDate = request.officialStartDate();
+        LocalDate endDate = startDate.plusDays(6);
+        int mod = days % 7;
+        int count = 1;
+        while (count <= weeks) {
+            for (Target t : dailyTargets) {
+                if (t.getStartDate().equals(endDate)) {
+                    weeklyTargets.add(
+                            Target.builder()
+                                    .timeFrame(TimeFrame.WEEK)
+                                    .startDate(startDate)
+                                    .endDate(t.getEndDate())
+                                    .estimatedBalanceByTargetAndTimeFrame(t.getEstimatedBalanceByTargetAndTimeFrame())
+                                    .variableEstimatedBalanceByTargetAndTimeFrame(t.getVariableEstimatedBalanceByTargetAndTimeFrame())
+                                    .build()
+                    );
+                    startDate = startDate.plusDays(7);
+                    endDate = endDate.plusDays(7);
+                    count++;
+                }
+            }
+        }
+        for (Target t : dailyTargets) {
+            if (t.getStartDate().equals(endDate.minusDays(7-mod)) & mod !=0) {
+                weeklyTargets.add(
+                        Target.builder()
+                                .timeFrame(TimeFrame.WEEK)
+                                .startDate(endDate)
+                                .endDate(t.getEndDate())
+                                .estimatedBalanceByTargetAndTimeFrame(t.getEstimatedBalanceByTargetAndTimeFrame())
+                                .variableEstimatedBalanceByTargetAndTimeFrame(t.getVariableEstimatedBalanceByTargetAndTimeFrame())
+                                .build()
+                );
+            }
+        }
+        targetRepository.saveAll(weeklyTargets);
+        return weeklyTargets;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(34%7);
+    }
+
+    //streams on calculateDailyTargets, filtering or grouping by months
+    public List<Target> calculateMonthlyTargets() {
         return null;
     }
 
-    public List<Target> calculateMonthlyTargets(){
+    //streams on calculateDailyTargets, filtering or grouping by years
+    public List<Target> calculateYearlyTargets() {
         return null;
     }
-    public List<Target> calculateYearlyTargets(){
-        return null;
-    }
+
+
 }
