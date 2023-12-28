bceee91be7d8e90af8f44e2c88750eb91c943311
achrafaitibba <aitibbaachraf@gmail.com>
2023-12-28 03:17:32 +0100

    h

--- src/main/java/com/achrafaitibba/trackcompoundingtrades/repository/AccountRepository.java
+++ src/main/java/com/achrafaitibba/trackcompoundingtrades/repository/AccountRepository.java
@@ -1,7 +1,6 @@
 package com.achrafaitibba.trackcompoundingtrades.repository;
 
 import com.achrafaitibba.trackcompoundingtrades.model.Account;
-import com.achrafaitibba.trackcompoundingtrades.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
